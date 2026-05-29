aa72ef0b2a69aec7b9e42e1863aabf60420c225b
chapman39 <chapman39@llnl.gov>
2026-05-28 17:51:18 -0700

    allow staging sphinx documentation, adds new arguments to `blt_add_sphinx_target`

--- cmake/SetupDocs.cmake
+++ cmake/SetupDocs.cmake
@@ -44,17 +44,42 @@ endmacro(blt_add_doxygen_target)
 
 
 ##------------------------------------------------------------------------------
-## blt_add_sphinx_target(sphinx_target_name)
+## blt_add_sphinx_target(sphinx_target_name
+##                       SOURCE_DIR   [source_dir]
+##                       CONF_DIR     [conf_dir]
+##                       DEPENDS      [dep1 ...])
 ##
 ## Creates a build target for invoking sphinx to generate docs
 ##------------------------------------------------------------------------------
-macro(blt_add_sphinx_target sphinx_target_name )
+macro(blt_add_sphinx_target sphinx_target_name)
+    set(_sphinx_options)
+    set(_sphinx_single_value_args
+        SOURCE_DIR
+        CONF_DIR)
+    set(_sphinx_multi_value_args
+        DEPENDS)
+    cmake_parse_arguments(SPHINX
+                          "${_sphinx_options}"
+                          "${_sphinx_single_value_args}"
+                          "${_sphinx_multi_value_args}"
+                          ${ARGN})
 
-    # configured documentation tools and intermediate build results
-    set(SPHINX_BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/_build")
+    if(SPHINX_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR
+            "blt_add_sphinx_target received unexpected arguments: ${SPHINX_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(NOT SPHINX_SOURCE_DIR)
+        set(SPHINX_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
+    endif()
+
+    # Path to Sphinx conf.py configuration file
+    if(NOT SPHINX_CONF_DIR)
+        set(SPHINX_CONF_DIR "${SPHINX_SOURCE_DIR}")
+    endif()
 
     # Sphinx cache with pickled ReST documents
-    set(SPHINX_CACHE_DIR "${CMAKE_CURRENT_BINARY_DIR}/_doctrees")
+    set(SPHINX_DOCTREE_DIR "${CMAKE_CURRENT_BINARY_DIR}/_doctrees")
 
     # HTML output directory
     set(SPHINX_HTML_DIR "${CMAKE_CURRENT_BINARY_DIR}/html")
@@ -62,35 +87,26 @@ macro(blt_add_sphinx_target sphinx_target_name )
     # support both direct use of a conf.py file and a cmake-configured
     # sphinx input file (conf.py.in). The cmake-configured input file is
     # preferred when both exist.
-    if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/conf.py.in")
-        configure_file("${CMAKE_CURRENT_SOURCE_DIR}/conf.py.in"
-                       "${SPHINX_BUILD_DIR}/conf.py"
+    if(EXISTS "${SPHINX_CONF_DIR}/conf.py.in")
+        set(new_conf_dir_ "${CMAKE_CURRENT_BINARY_DIR}/_conf")
+        configure_file("${SPHINX_CONF_DIR}/conf.py.in"
+                       "${new_conf_dir_}/conf.py"
                        @ONLY)
-
-        add_custom_target(${sphinx_target_name}
-                          ${SPHINX_EXECUTABLE}
-                          -q -b html
-                          #-W disable warn on error for now, while our sphinx env is still in flux
-                          -c "${SPHINX_BUILD_DIR}"
-                          -d "${SPHINX_CACHE_DIR}"
-                          "${CMAKE_CURRENT_SOURCE_DIR}"
-                          "${SPHINX_HTML_DIR}"
-                          COMMENT "Building HTML documentation with Sphinx for ${sphinx_target_name} target"
-                          DEPENDS ${deps})
-    elseif(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/conf.py")
-        add_custom_target(${sphinx_target_name}
-                          ${SPHINX_EXECUTABLE}
-                          -q -b html
-                          #-W disable warn on error for now, while our sphinx env is still in flux
-                          -d "${SPHINX_CACHE_DIR}"
-                          "${CMAKE_CURRENT_SOURCE_DIR}"
-                          "${SPHINX_HTML_DIR}"
-                          COMMENT "Building HTML documentation with Sphinx for ${sphinx_target_name} target"
-                          DEPENDS ${deps})
-    else()
-        message(FATAL_ERROR "Failed to find sphinx 'conf.py' or 'conf.py.in' in ${CMAKE_CURRENT_SOURCE_DIR}")
+        set(SPHINX_CONF_DIR "${new_conf_dir_}")
+        unset(new_conf_dir_)
     endif()
-        
+
+    add_custom_target(${sphinx_target_name}
+        COMMAND ${SPHINX_EXECUTABLE}
+                -q
+                -b html
+                -c "${SPHINX_CONF_DIR}"
+                -d "${SPHINX_DOCTREE_DIR}"
+                "${SPHINX_SOURCE_DIR}"
+                "${SPHINX_HTML_DIR}"
+        COMMENT "Building HTML documentation with Sphinx for ${sphinx_target_name} target"
+        DEPENDS ${SPHINX_DEPENDS})
+
     # hook our new target into the docs dependency chain
     add_dependencies(sphinx_docs ${sphinx_target_name})
 
--- docs/api/documentation.rst
+++ docs/api/documentation.rst
@@ -30,18 +30,58 @@ blt_add_sphinx_target
 
 .. code-block:: cmake
 
-    blt_add_sphinx_target(sphinx_target_name)
+    blt_add_sphinx_target(sphinx_target_name
+                          SOURCE_DIR [source_dir]
+                          CONF_DIR [conf_dir]
+                          DEPENDS [dep1 ...])
 
-Creates a build target for invoking Sphinx to generate docs. Expects
-to find a ``conf.py`` or ``conf.py.in`` in the directory the macro is called
-in. Requires that a CMake variable named ``SPHINX_EXECUTABLE``
+Creates a custom build target that invokes Sphinx to generate HTML
+documentation. Requires that a CMake variable named ``SPHINX_EXECUTABLE``
 contains the path to the ``sphinx-build`` executable.
 
-If ``conf.py`` is found, it is directly used as input to Sphinx.
+The macro has the following optional arguments:
 
-If ``conf.py.in`` is found, this macro uses CMake's ``configure_file()`` command
-to generate a ``conf.py``, which is then used as input to Sphinx.
+* ``SOURCE_DIR``, the Sphinx source directory, defaulting to
+  ``${CMAKE_CURRENT_SOURCE_DIR}``
+* ``CONF_DIR``, the directory containing ``conf.py`` or ``conf.py.in``,
+  defaulting to ``SOURCE_DIR``
+* ``DEPENDS``, additional CMake dependencies for the Sphinx target
+
+If ``CONF_DIR/conf.py.in`` exists, CMake configures it to generate a
+``conf.py`` in the build tree and uses that configured file for the Sphinx
+build. If ``conf.py.in`` is not present, the macro uses the existing
+``conf.py`` in ``CONF_DIR`` directly.
 
 This macro sets up the sphinx paths so that the doc builds happen
 out of source. For ``make install``, this will place the resulting docs in
 ``docs/sphinx/<sphinx_target_name>``.
+  
+Staging Example
+-----------------
+
+A stage directory is useful when the Sphinx input tree must be assembled from
+multiple source locations, such as when one documentation project is a subset
+of another. In this case, the stage step copies shared files and local docs
+into a temporary tree, which is then passed to Sphinx as ``SOURCE_DIR``.
+
+Let's say project A depends on project B.
+
+.. code-block:: cmake
+
+    set(PROJECT_A_STAGE_DIR "${CMAKE_CURRENT_BINARY_DIR}/_stage")
+    set(PROJECT_A_SOURCE_DIR "${PROJECT_A_STAGE_DIR}/src/docs")
+
+    add_custom_target(stage_my_docs
+      COMMAND ${CMAKE_COMMAND} -E remove_directory "${PROJECT_A_STAGE_DIR}"
+      COMMAND ${CMAKE_COMMAND} -E make_directory "${PROJECT_A_STAGE_DIR}"
+      COMMAND ${CMAKE_COMMAND} -E copy_directory "${PROJECT_B_SOURCE_DIR}/src" "${PROJECT_A_STAGE_DIR}/src"
+      COMMAND ${CMAKE_COMMAND} -E copy_directory "${PROJECT_B_SOURCE_DIR}/examples" "${PROJECT_A_STAGE_DIR}/examples"
+      COMMAND ${CMAKE_COMMAND} -E copy_directory "${PROJECT_B_SOURCE_DIR}/tests" "${PROJECT_A_STAGE_DIR}/tests"
+      COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_CURRENT_SOURCE_DIR}" "${PROJECT_A_SOURCE_DIR}"
+      COMMENT "Staging documentation from project A and B sources"
+      VERBATIM)
+
+    blt_add_sphinx_target(my_docs
+      SOURCE_DIR "${PROJECT_A_SOURCE_DIR}"
+      CONF_DIR "${CMAKE_CURRENT_SOURCE_DIR}"
+      DEPENDS stage_my_docs)
